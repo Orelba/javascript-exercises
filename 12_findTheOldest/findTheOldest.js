2a3e5f4e2c4573e92f1e405ed063d7d72030ebdb
Orelba <orel450@gmail.com>
2022-02-24 19:45:15 +0200

    Finished exercise 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(peopleArr) {
+  peopleArr.forEach(person => {
+    if (person.yearOfDeath) {
+      person.age = person.yearOfDeath - person.yearOfBirth
+    } else {
+      person.age = new Date().getFullYear() - person.yearOfBirth
+    }
+  });
+  const ordered = peopleArr.sort((a, b) => a.age < b.age ? 1 : -1)
+  return ordered[0]
 };
 
 // Do not edit below this line
