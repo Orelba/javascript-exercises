cce117e2ee28c55c2a472cadb65e0b94c84b0add
Orelba <orel450@gmail.com>
2022-02-24 16:07:45 +0200

    Finished exercise 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  const lowerCaseString = str.toLowerCase()
+  const puncLessString = lowerCaseString.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"") // Remove puctuation
+  const cleanString = puncLessString.replace(/\s/g,"") // Remove spaces
+  const reversedString = Array.from(cleanString).reverse().join('')
 
+  if (cleanString === reversedString) return true
+  return false
 };
 
 // Do not edit below this line
